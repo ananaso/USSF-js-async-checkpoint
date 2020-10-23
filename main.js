1c5414f4f41f5f97af97be9a35e0049281eb0765
Alden Davidson <adavidson+github@protonmail.ch>
2020-10-23 14:13:57 -0700

    program now correctly logs pokemon and types

--- main.js
+++ main.js
@@ -7,26 +7,25 @@ const fetch = require('node-fetch');
 
 // read in pokemon names
 let names = fs.readFileSync('input.txt').toString().split('\n');
-console.log(names);
 
-names = names.forEach(pokemon => {
-    let capitalName = pokemon.charAt(0).toUpperCase() + pokemon.slice(1);
-    let entry = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+// get all types from pokeapi
+let types = [];
+names.forEach(pokemon => {
+    types.push(fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then(response => response.json())
     .then(data => data.types.map(slot => {
         return slot.type.name;
     }))
     .then(types => {
         return `${types.join(', ')}`;
-    });
-}).then(names => {
-    console.log(names);
+    }));
 })
 
-// booo, doesn't work for some reason
-// yet maybe we need to save the names to a variable not sure
-// no, doesn't seem like it: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-// I think we just need to find some way to wait for the fetches to finish
-// reemove the capitalName
-// i cant run the terminal im in read only mode
-// ahh, okay. I'll push it all.
+// after all types have been fetched
+Promise.all(types)  // resolve the promises
+    .then(types => {
+        names.forEach((pokemon, indx) => {
+            let capitalName = pokemon.charAt(0).toUpperCase() + pokemon.slice(1);   // capitalize first letter of pokemon name
+            console.log(`${capitalName}: ${types[indx]}`);  // log pokemon name with types in defined format
+        })
+    });
